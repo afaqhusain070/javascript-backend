03516d881d068f5f779396d14eb81290c36d865e
Ali Muhammadi <ali@trade2simple>
2024-05-20 14:28:40 +0500

    add new routes for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser , refreshAccessToken} from "../controllers/user.contoller";
+import { registerUser, loginUser, logoutUser , refreshAccessToken, changeCurrentpassword, getCurrentUser, updateUserAvatar, UserUpdateDetail, updateUserCoverImage, getUserChannelProfile, getWatchHistory} from "../controllers/user.contoller";
 const router = Router()
 import { verifyJWT } from "../middlewares/auth.middleware";
 import { upload } from "../middlewares/multer.middleware";
@@ -15,6 +15,12 @@ router.route("/login").post(loginUser)
 // secired routes
 router.route("/logout").post(verifyJWT , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-
+router.route("/change-passwrod").post(verifyJWT, changeCurrentpassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, UserUpdateDetail)
+router.route("/update-avator").patch(verifyJWT, upload.single("avator"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
